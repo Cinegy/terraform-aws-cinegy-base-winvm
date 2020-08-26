c9ac63b4cbceb9f23a58530ee016ddb47d692c4e
Lewis Kirkaldie <lewis@cinegy.com>
2020-08-26 17:14:28 +0100

    -correct listing of providers

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 2.7.0"
-    },
+    }
     tls = {
       source  = "hashicorp/aws"
       version = ">= 2.2"
-    },
+    }
     template = {
       source  = "hashicorp/aws"
       version = ">= 2.1.2"
