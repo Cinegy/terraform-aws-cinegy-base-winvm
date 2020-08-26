5d6be1a5499637f559cd59b7fa4b137adb463d2d
Lewis Kirkaldie <lewis@cinegy.com>
2020-08-26 17:19:20 +0100

    -stupid provider error

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 terraform {
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
       version = ">= 2.7.0"
     }
+
     tls = {
-      source  = "hashicorp/aws"
+      source  = "hashicorp/tls"
       version = ">= 2.2"
     }
+
     template = {
-      source  = "hashicorp/aws"
+      source  = "hashicorp/template"
       version = ">= 2.1.2"
     }
   }
