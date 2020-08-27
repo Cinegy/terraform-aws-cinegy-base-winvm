fdac93b8888107dd3025e955d85784806ea26f24
Lewis Kirkaldie <lewis@cinegy.com>
2020-08-27 09:04:22 +0100

    -add telemetry and AWS license to userdata script

--- conf/userdatascriptbase.ps1
+++ conf/userdatascriptbase.ps1
@@ -1,5 +1,9 @@
 #Requires -RunAsAdministrator
 
+$licenseserverurl = "https://central.cinegy.com/api_awsmrkt/v1/license"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$telemetryServerUrl = "https://telemetry.cinegy.com"
+
 function RenameHost(){
 	
 	#Perform rename using 'hostname' tag from AWS metadat
@@ -51,6 +55,102 @@ function Install-DefaultPackages(){
 	Install-Product -PackageName Thirdparty-MetricBeat-v6.x -VersionTag dev
 	Install-Product -PackageName Thirdparty-NotepadPlusPlus-v7.x -VersionTag prod
 	Install-Product -PackageName Thirdparty-Firefox-Stable -VersionTag prod
+}
+
+function Send-TelemetryMessage([string] $Message, [string] $IndexName, [string] $OrganizationName="cinegy", [string] $Level="Info" ,[string] $tags="aws,production")
+{
+	#create an object of correct structure to steer the log record in ElasticSearch (this will be first line sent to server)
+	$index = [PSCustomObject]@{
+		index = [PSCustomObject]@{
+			_index = "$IndexName-$OrganizationName-$([System.DateTime]::UtcNow.Year).$([System.DateTime]::UtcNow.Month.ToString("00")).$([System.DateTime]::UtcNow.Day.ToString("00"))"
+			_type = "logevent"
+			}
+	}
+
+	#create an object of minimum structure to send to ElasticSearch (this will be the second line sent to server)
+	$log = [PSCustomObject]@{
+		Level = $Level
+		Time = Get-Date -format o
+		Tags = $tags
+		Host = [system.environment]::MachineName
+		Logger = "PowershellTelemetryScript"
+		Key = "Message"
+		Message = $Message
+	}
+
+	#convert the custom objects to JSON format, with each object on a single line
+	$body = (ConvertTo-Json -InputObject $index -Compress) + "`n" + (ConvertTo-Json -InputObject $log -Compress)
+
+	#print resulting string to console
+	Write-Host $body
+
+	#create webclient object, add JSON content-type header, and POST to telemetry server
+	$web = new-object net.webclient
+	$web.Headers.add("Content-Type", "application/json")
+	$web.UploadString("$telemetryServerUrl/_bulk",$body)
+}
+
+function Get-StringHash([string] $String,[string]$HashName="MD5")
+{
+    $StringBuilder=New-Object System.Text.StringBuilder
+    [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String)) |
+		ForEach-Object { [Void]$StringBuilder.Append($_.ToString("x2"))}
+    return $StringBuilder.ToString()
+}
+
+function Get-CinegyMachineFingerprint($UseTaggedHostname = $true)
+{
+    $fingerprintVersion = "01"
+    $cpuKey = Get-ItemProperty "HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\0"
+    $biosKey = Get-ItemProperty "HKLM:\HARDWARE\DESCRIPTION\System\BIOS"
+
+	$computerName = $env:computername
+	if($UseTaggedHostname) { $computerName = Get-LocalInstanceTagValue("Hostname") }
+
+    $fingerprintRaw = "$($computerName)-#-$($cpuKey.Identifier)-#-$($biosKey.BaseBoardProduct)-#-$($biosKey.BaseBoardManufacturer)-#-$($biosKey.BIOSVendor)".Replace(" ","").ToLowerInvariant();
+
+    $hashedPrint = Get-StringHash($fingerprintRaw) 
+    return "$($hashedPrint)$fingerprintVersion"
+}
+
+function Get-AwsSignedMetadata()
+{
+  try #read AWS metadata from instance
+  {
+      $metadataUrl = "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7"
+      $signedMetadata = Invoke-RestMethod -UseBasicParsing  -Method Get -Uri $metadataUrl  
+      return $signedMetadata
+  }
+  catch
+  {
+      Write-Output "Problem getting AWS Instance Metadata: $PSItem"
+	  Send-TelemetryMessage -Message "Problem getting AWS Instance Metadata: $PSItem" -IndexName "awsmarketplace" -Level "Error"
+  }
+}
+
+function Get-AwsLicense($UseTaggedHostname = $false)
+{
+  # note - this will only work if the account ID or instance ID has a back-end record, 
+  # or if there is a valid Cinegy marketplace code embedded in the signedmetadata - otherwise this will fail
+
+  try{   	  
+	  Write-Output "Getting license file from server URL: $licenseserverurl"
+      $headers = @{'accept'="application/xml";'content-type'="application/json"}
+      $signedMetadata = Get-AwsSignedMetadata 
+	  $encodedSignedMetadata = [System.Web.HttpUtility]::UrlEncode($signedMetadata)
+      $fingerprint = Get-CinegyMachineFingerprint -UseTaggedHostname $UseTaggedHostname
+	  $encodedfingerprint = [System.Web.HttpUtility]::UrlEncode($fingerprint)
+	  $uri = "$licenseserverurl/negotiate?MachineId=$encodedfingerprint&AwsInstanceMetadata=$encodedSignedMetadata"
+	  
+	  New-Item -ItemType Directory -Path "C:\ProgramData\Cinegy\Software Licenses\" -Force -ErrorAction SilentlyContinue | Out-Null
+
+      Invoke-RestMethod -UseBasicParsing -Method Post -Uri $uri -Headers $headers -OutFile "C:\ProgramData\Cinegy\Software Licenses\aws-instance-license.cinelic"   
+  }
+  catch
+  {
+      Write-Output "Problem negotiating license from license server: $PSItem"
+	  Send-TelemetryMessage -Message "Problem getting license key from license server: $PSItem" -IndexName "awsmarketplace" -Level "Error"
+  }
 }
 
 function Get-LocalInstanceTagValue([string] $tagName)
